36ea1728817bf3673c039374e10cda58445ab868
491055741 <491055741@qq.com>
2015-09-04 17:17:27 +0800

    v1.1b26 channel: anhui ; fix bug: change password

--- src/com/coolwifi/main/MainActivity.java
+++ src/com/coolwifi/main/MainActivity.java
@@ -319,7 +319,6 @@ public class MainActivity extends AppCompatActivity {
         }
         
 	    initCustomActionBar();
-	    AnalyticsConfig.setChannel("channel");
 
 	    registerWIFI();
 		registerConnection();
